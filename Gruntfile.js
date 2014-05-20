c1bf1492ca7917729a833339e206be40bcb58f7f
Mario Carballo Zama <arlefreak@gmail.com>
2014-05-19 22:38:19 -0500

    Grunt files updated

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,84 +1,83 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
     // 1. All configuration goes here 
     grunt.initConfig({
-	pkg: grunt.file.readJSON('package.json'),
-	watch :{
-	    scripts :{
-		files : 'js/',
-		tasks : ['concat','uglify'],
-		options : {
-		    livereload : 9090,
-		}
-	    }
-	},	
-	concat: {
-	    options: {
-		separator: ';',
-	    },
-	    dist: {
-		src: [
-		    'js/lib/Phaser.js',
-		    'js/lib/Helpers.js',
-		    'js/lib/Underscore.js',
-		    'js/src/Prefabs/Grid.js',
-		    'js/src/Prefabs/GridLayer.js',
-		    'js/src/Prefabs/Enemy.js',
-		    'js/src/States/Boot.js',
-		    'js/src/States/Preloader.js',
-		    'js/src/States/MainMenu.js',
-		    'js/src/States/Credits.js',
-		    'js/src/States/LeaderBoards.js',
-		    'js/src/States/Play.js',
-		    'js/src/Main.js'],
-		    dest: 'build/CRSG.js',
-	    },
-	},
-	replace: {
-	    example: {
-		src: ['build/CRSG.js'],             // source files array (supports minimatch)
-		dest: 'build/CRSG.js',             // destination directory or file
-		replacements: [{
-		    from: 'js/res/',                   // string replacement
-		    to: '/static/Games/CRSG/assets/'
-		}]
-	    }            
-	},  
-	uglify: {
-	    build: {
-		src: 'build/CRSG.js',
-		dest: 'build/CRSG.min.js'
-	    }
-	},
-	imagemin: {
-	    dynamic: {
-		files: [{
-		    expand: true,
-		    cwd: 'js/res/',
-		    src: ['**/*.{png,jpg,gif}'],
-		    dest: 'build/assets/'
-		}]
-	    }
-	},
-	copy: {
-	    main: {
-		files: [{
-		    expand: true,
-		    cwd: 'js/res/',
-		    src: ['**/*.{json,mp3,ogg,wav}'],
-		    dest: 'build/assets/'
-		},
-		{
-		    expand: true,
-		    cwd: 'js/res/',
-		    src: ['fonts/**'],
-		    dest: 'build/assets/'
-		},
-		]
-	    }
-	},
+        pkg: grunt.file.readJSON('package.json'),
+        watch: {
+            scripts: {
+                files: 'js/',
+                tasks: ['concat', 'uglify'],
+                options: {
+                    livereload: 9090,
+                }
+            }
+        },
+        concat: {
+            options: {
+                separator: ';',
+            },
+            dist: {
+                src: [
+                    'js/lib/Phaser.js',
+                    'js/lib/Helpers.js',
+                    'js/lib/Underscore.js',
+                    'js/lib/SolverNode.js',
+                    'js/lib/Solver.js',
+                    'js/src/Prefabs/Grid.js',
+                    'js/src/Prefabs/GridLayer.js',
+                    'js/src/Prefabs/Enemy.js',
+                    'js/src/States/Boot.js',
+                    'js/src/States/Preloader.js',
+                    'js/src/States/MainMenu.js',
+                    'js/src/States/Credits.js',
+                    'js/src/States/LeaderBoards.js',
+                    'js/src/States/Play.js',
+                    'js/src/Main.js'
+                ],
+                dest: 'build/CRSG.js',
+            },
+        },
+        replace: {
+            example: {
+                src: ['build/CRSG.js'], // source files array (supports minimatch)
+                dest: 'build/CRSG.js', // destination directory or file
+                replacements: [{
+                    from: 'js/res/', // string replacement
+                    to: '/static/Games/CRSG/assets/'
+                }]
+            }
+        },
+        uglify: {
+            build: {
+                src: 'build/CRSG.js',
+                dest: 'build/CRSG.min.js'
+            }
+        },
+        imagemin: {
+            dynamic: {
+                files: [{
+                    expand: true,
+                    cwd: 'js/res/',
+                    src: ['**/*.{png,jpg,gif}'],
+                    dest: 'build/assets/'
+                }]
+            }
+        },
+        copy: {
+            main: {
+                files: [{
+                    expand: true,
+                    cwd: 'js/res/',
+                    src: ['**/*.{json,mp3,ogg,wav}'],
+                    dest: 'build/assets/'
+                }, {
+                    expand: true,
+                    cwd: 'js/res/',
+                    src: ['fonts/**'],
+                    dest: 'build/assets/'
+                }, ]
+            }
+        },
     });
-
-
 
     // 3. Where we tell Grunt we plan to use this plug-in.
     grunt.loadNpmTasks('grunt-contrib-concat');
@@ -86,6 +85,6 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-imagemin');
     grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.registerTask('default', ['concat','replace','uglify','imagemin','copy']);
+    grunt.registerTask('default', ['concat', 'replace', 'uglify', 'imagemin', 'copy']);
 
-}; 
+};
