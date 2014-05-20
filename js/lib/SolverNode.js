c73b372fc9dd41da2e8fe4be1e67627110de6899
Mario Carballo Zama <arlefreak@gmail.com>
2014-05-19 22:29:12 -0500

    SolverNode class cleaned and commented

--- js/lib/SolverNode.js
+++ js/lib/SolverNode.js
@@ -1,11 +1,10 @@
 /*
- * SolverNode
- * Author: Arlefreak
- * */
-
+* SolverNode
+* Author: Arlefreak
+* */
 'use strict';
 
-var SolverNode = function (_matrix, _parent, _movableX, _movableY, _direction,_totalCost,_moves) {
+var SolverNode = function (_matrix, _parent, _movableX, _movableY, _direction, _totalCost, _moves) {
     this.matrix = _matrix;
     this.parent = _parent;
     this.movableX = _movableX;
@@ -17,60 +16,67 @@ var SolverNode = function (_matrix, _parent, _movableX, _movableY, _direction,_t
 };
 
 
-SolverNode.prototype.calcHueristic = function(){
-    var h = 0;
-    var i = 0;
-    var j = 0;
+SolverNode.prototype.calcHueristic = function () {
+    var h = 0;/* The new heuristic */
+    var i = 0;/* Loop variable */
+    var j = 0;/* Loop variable */
+    
+    /* Objective index */
     var playerIndexX = 0;
     var playerIndexY = 0;
+
+    /* The index wich are going to ve evaluated */
     var initialIndex = 0;
     var finalIndex = 0;
 
-    for (i = this.matrix.length - 1; i >= 0; i--){
-	for (j = this.matrix[i].length - 1; j >= 0; j--){
-	    if(this.matrix[i][j] === 5){
-		playerIndexX = i;
-		playerIndexY = j;
-	    }
-	}
+    /* Get the player index */
+    for (i = this.matrix.length - 1; i >= 0; i--) {
+        for (j = this.matrix[i].length - 1; j >= 0; j--) {
+            if (this.matrix[i][j] === 5) {
+                playerIndexX = i;
+                playerIndexY = j;
+            }
+        }
     }
 
-    if(this.movableX > playerIndexX){
-	initialIndex = playerIndexX;
-	finalIndex = this.movableX;
-    }else{
-	initialIndex = this.movableX;
-	finalIndex = playerIndexX;
+    /* Check wich one is going to be the intial and final X index */
+    if (this.movableX > playerIndexX) {
+        initialIndex = playerIndexX;
+        finalIndex = this.movableX;
+    } else {
+        initialIndex = this.movableX;
+        finalIndex = playerIndexX;
     }
 
-    for(i = finalIndex; i>= initialIndex; i--){
-	if(this.matrix[i][this.movableY] !== 0 || this.matrix[i][this.movableY] !== 9){
-	    h++;
-	}    
+    /* Add the obstacles on between the two ondex */
+    for (i = finalIndex; i >= initialIndex; i--) {
+        if (this.matrix[i][this.movableY] !== 0 || this.matrix[i][this.movableY] !== 9) {
+            h++;
+        }
     }
 
-    if(this.movableiY > playerIndexY){
-	initialIndex = playerIndexY;
-	finalIndex = this.movableY;
-    }else{
-	initialIndex = this.movableY;
-	finalIndex = playerIndexY;
+    /* Check wich one is going to be the intial and final Y index */
+    if (this.movableiY > playerIndexY) {
+        initialIndex = playerIndexY;
+        finalIndex = this.movableY;
+    } else {
+        initialIndex = this.movableY;
+        finalIndex = playerIndexY;
     }
 
-    for(i = finalIndex; i>= initialIndex; i--){
-	if(this.matrix[this.movableX][i] !== 0 || this.matrix[this.movableX][i] !== 9){
-	    h++;
-	}    
+    /* Add the obstacles on between the two ondex */
+    for (i = finalIndex; i >= initialIndex; i--) {
+        if (this.matrix[this.movableX][i] !== 0 || this.matrix[this.movableX][i] !== 9) {
+            h++;
+        }
     }
 
-
+    /* Add the distance between the two index */
     h += Math.abs(this.movableX - playerIndexX);
     h += Math.abs(this.movableY - playerIndexY);
-    this.heuristic =  h;
+    this.heuristic = h;
 };
 
-SolverNode.prototype.calcTotalCost = function(){
+SolverNode.prototype.calcTotalCost = function () {
     this.totalCost = this.moves + this.heuristic;
 };
-
-
